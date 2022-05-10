d037582dd8a0a9166d1307a1b4c78aacee1ebe78
Fulia <80128190+SiddharthFulia@users.noreply.github.com>
2022-05-10 15:51:39 +0530

    Add files via upload

---
+++ build/precache-manifest.fd36ddb4483b9cd31513906249a9d60d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4610146e64833bf860e527e27da5ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2032402838c42c0f207c",
+    "url": "/static/css/main.bdf543ac.chunk.css"
+  },
+  {
+    "revision": "44770abcd4a12a3e0075",
+    "url": "/static/js/2.74e666b0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.74e666b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8170d604a84a93f1afd8",
+    "url": "/static/js/3.5ca29e66.chunk.js"
+  },
+  {
+    "revision": "2032402838c42c0f207c",
+    "url": "/static/js/main.8d5f5283.chunk.js"
+  },
+  {
+    "revision": "8de7f790ed3ccfac9477",
+    "url": "/static/js/runtime-main.568b1885.js"
+  }
+]);
